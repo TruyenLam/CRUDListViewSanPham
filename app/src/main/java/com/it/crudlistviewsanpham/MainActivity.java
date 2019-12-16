802d947dd8c281f576cdddedc03cdd3c7ef0284e
TruyenLam <lamvantruyen@gmail.com>
2019-12-16 11:23:39 +0700

    code button add san pham

--- app/src/main/java/com/it/crudlistviewsanpham/MainActivity.java
+++ app/src/main/java/com/it/crudlistviewsanpham/MainActivity.java
@@ -3,7 +3,9 @@ package com.it.crudlistviewsanpham;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,15 +17,31 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<sanpham> arraySanpham;
     sanphamAdapter adapter;
     Button btnThem,btnXoa,btnSua;
+    EditText edtMasp,edtTensp,edtSoluong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         load();
+        addsanpham();
 
         adapter = new sanphamAdapter(this,R.layout.sanpham_view, arraySanpham);
         lvSanpham.setAdapter((adapter));
+    }
+
+    private void addsanpham() {
+        btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int sl;
+                sl =Integer.parseInt(edtSoluong.getText().toString());
+                arraySanpham.add(new sanpham(edtMasp.getText().toString(),
+                        edtTensp.getText().toString(),sl));
+
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 
     private void load() {
@@ -31,6 +49,9 @@ public class MainActivity extends AppCompatActivity {
         btnThem = (Button) findViewById(R.id.buttonThem);
         btnSua=(Button) findViewById(R.id.buttonSua);
         btnXoa=(Button) findViewById(R.id.buttonXoa);
+        edtMasp=(EditText) findViewById(R.id.editTextMasp);
+        edtTensp=(EditText) findViewById(R.id.editTextTensp);
+        edtSoluong=(EditText) findViewById(R.id.editTextSoluong);
 
         arraySanpham = new ArrayList<>();
 
---
+++ app/src/main/java/com/it/crudlistviewsanpham/sanpham_add.java
@@ -0,0 +1,14 @@
+package com.it.crudlistviewsanpham;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sanpham_add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sanpham_add);
+    }
+}
